2cfb666323708c58052a678b50bb8730f2330d54
BayuDC <dev.bayudc@gmail.com>
2022-04-16 05:18:37 +0700

    Implement authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller {
     public function index() {
@@ -13,7 +14,21 @@ class AuthController extends Controller {
             'username' => 'required',
             'password' => 'required'
         ]);
+
+        if (Auth::attempt($validated)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->with('msg_error', 'Login failed!')->onlyInput('username');
     }
-    public function logout() {
+    public function logout(Request $request) {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/logout', [AuthController::class, 'logout']);
