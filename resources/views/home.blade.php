3445b86ba9bde3011c74aabb671a5094ee018881
BayuDC <dev.bayudc@gmail.com>
2022-04-15 13:22:57 +0700

    Add view layouts with bulma

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+<h1 class="title">
+    {{ config('app.name') }}
+</h1>
+<p class="subtitle">
+    My first website with <strong>Bulma</strong>!
+</p>
+@endsection
